0480af5bcf7672ae9c38942d1504142ed75c082b
jzyGithub <18519764901@163.com>
2018-07-21 23:46:37 +0800

    ckeditor 更换为4.4.1版本为了增加代码高亮

--- src/main/resources/static/ckeditor/config.js
+++ src/main/resources/static/ckeditor/config.js
@@ -1,43 +1,72 @@
-/**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For the complete reference:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
-
-	// Remove some buttons, provided by the standard plugins, which we don't
-	// need to have in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
-
-	// Se the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Make dialogs simpler.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
-	//超链接文件上传请求路径(没生效，后续待测试)
-    //config.filebrowserUploadUrl = "/back/image/fileUpload"
-    config.removeDialogTabs = 'image:advanced;image:Link';//隐藏超链接与高级选项
-	//工具栏“图像”中预览区域显示内容
-	config.image_previewText = '图片最大宽度最好设置为760px';
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	// %REMOVE_START%
+	config.plugins =
+		'about,' +
+		'a11yhelp,' +
+		'basicstyles,' +
+		'bidi,' +
+		'blockquote,' +
+		'clipboard,' +
+		'colorbutton,' +
+		'colordialog,' +
+		'contextmenu,' +
+		'dialogadvtab,' +
+		'div,' +
+		'elementspath,' +
+		'enterkey,' +
+		'entities,' +
+		'filebrowser,'+
+		'find,' +
+		'flash,' +
+		'floatingspace,' +
+		'font,' +
+		'format,' +
+		'forms,' +
+		'horizontalrule,' +
+		'htmlwriter,' +
+		'image,' +
+		'iframe,' +
+		'indentlist,' +
+		'indentblock,' +
+		'justify,' +
+		'language,' +
+		'link,' +
+		'list,' +
+		'liststyle,' +
+		'magicline,' +
+		'maximize,' +
+		'newpage,' +
+		'pagebreak,' +
+		'pastefromword,' +
+		'pastetext,' +
+		'preview,' +
+		'print,' +
+		'removeformat,' +
+		'resize,' +
+		'save,' +
+		'selectall,' +
+		'showblocks,' +
+		'showborders,' +
+		'smiley,' +
+		'sourcearea,' +
+		'specialchar,' +
+		'stylescombo,' +
+		'tab,' +
+		'table,' +
+		'tabletools,' +
+		'templates,' +
+		'toolbar,' +
+		'undo,' +
+		'wysiwygarea';
+	// %REMOVE_END%
 };
+
+// %LEAVE_UNMINIFIED% %REMOVE_LINE%
